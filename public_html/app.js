e604bd7169614aff0b39026535fcbbfb5df5c119
Allen Garvey <allen.garvey@gmail.com>
2018-09-13 17:08:42 -0400

    Reduce redundancy needed for multiple staves

--- public_html/app.js
+++ public_html/app.js
@@ -15,27 +15,21 @@ M:3/8
 L:1/8
 Q:200
 %%staves {1 2}
-K:C`,
+K:C
+V:1 clef=treble
+V:2 clef=bass`,
         measures: [
-`V:1
-[K:C clef=treble]fdg|
-V:2
-[K:C clef=bass]F,D,G,|`,
+`[V:1]fdg|
+[V:2]F,D,G,|`,
 
-`V:1
-[K:C clef=treble]A^F/2G/2B/2g/2|
-V:2
-[K:C clef=bass][B,,2 G,2] z|`,
+`[V:1]A^F/2G/2B/2g/2|
+[V:2][B,,2 G,2] z|`,
 
-`V:1
-[K:C clef=treble]gce|
-V:2
-[K:C clef=bass][C,2 E,2] z|`,
+`[V:1]gce|
+[V:2][C,2 E,2] z|`,
 
-`V:1
-[K:C clef=treble]g d2|
-V:2
-[K:C clef=bass]G,,/2B,,/2G,B,,|`,
+`[V:1]g d2|
+[V:2]G,,/2B,,/2G,B,,|`,
 ],
 };
 }
